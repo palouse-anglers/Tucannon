7618c483d22f899ecbb84c44175e5ca26a120e4f
Louis M. Penrod <louismpenrod@gmail.com>
2025-05-04 18:17:59 +0000

    Add iframe module

---
+++ R/iframe_mod.R
@@ -0,0 +1,30 @@
+iframeUI <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::uiOutput(ns("iframe"))
+}
+ 
+
+iframeServer <- function(id, url, style) {
+  shiny::moduleServer(id, function(input, output, session) {
+    output$iframe <- shiny::renderUI({
+      shiny::tags$iframe(
+        src = url,
+        style= style
+      )
+    })
+  })
+}
+
+
+# usage
+ui <- shiny::fluidPage(
+  iframeUI("myiframe")
+)
+
+server <- function(input, output, session) {
+  iframeServer("myiframe", 
+               url = "https://www.wikipedia.org", 
+               style = 'width:90vw;height:90vh;')
+}
+
+shiny::shinyApp(ui, server)
